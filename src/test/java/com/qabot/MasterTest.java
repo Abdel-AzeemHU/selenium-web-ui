268669d0ef4aca44f24bd9078a0ec9808dc3c4c3
AbdelazeemHU <54842309+AbdelazeemHU@users.noreply.github.com>
2021-12-23 23:28:14 +0200

    Init Master Test class

--- src/test/java/com/qabot/MasterTest.java
+++ src/test/java/com/qabot/MasterTest.java
@@ -1,46 +1,25 @@
 package com.qabot;
 
+import com.qabot.utils.Driver;
+import com.qabot.utils.PropertyReader;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.time.Duration;
-import java.util.Properties;
 
 
 public class MasterTest {
 
-    public WebDriver driver;
+    protected WebDriver driver;
 
-    public WebDriver initializeDriver() throws IOException {
-
-        // Chrome driver
-        Properties prop = new Properties();
-        FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/properties/userdata.properties");
-        prop.load(file);
-        String browserName = prop.getProperty("browser");
-        System.out.println(browserName);
-
-        if (browserName.equals("chrome")) {
-            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
-            driver = new ChromeDriver();
-
-        } else if (browserName.equals("firefox")) {
-            // Firefox code
-            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
-            driver = new ChromeDriver();
-
-        } else if (browserName.equals("IE")) {
-            // IE code
-            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
-            driver = new ChromeDriver();
-        }
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        return driver;
+    @BeforeClass
+    public void beforeClass() {
+        driver = Driver.createDriver();
+        driver.navigate().to(PropertyReader.getProperty("baseURL"));
     }
 
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
 }
 
 
