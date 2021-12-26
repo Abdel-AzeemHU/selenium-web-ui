19515a5a90f846ba4413be5b19d035a3ed05f6ea
AbdelazeemHU <abdelazeemhu@gmail.com>
2021-12-26 19:06:18 +0200

    some clean

--- src/test/java/com/qabot/ContextMenuTest.java
+++ src/test/java/com/qabot/ContextMenuTest.java
@@ -1,8 +1,6 @@
 package com.qabot;
 
-import com.qabot.pages.CheckBoxesPage;
 import com.qabot.pages.ContextMenuPage;
-import com.qabot.pages.HomePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,7 +8,6 @@ public class ContextMenuTest extends MasterTest{
 
     @Test(description = "Click context menu  ")
     public void performRightClickAndAcceptTheAlert() {
-        HomePage homePage = new HomePage(driver);
         ContextMenuPage contextMenuPage = homePage.clickLinkByText("Context Menu", ContextMenuPage.class);
         contextMenuPage.rightClickAndAcceptTheAlert();
         Assert.assertTrue(contextMenuPage.contextMenuHeaderTxt.getText().contains("Context Menu"));
